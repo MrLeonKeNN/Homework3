0e69e5fcf6e705058514cf91a150bd70745a480b
v.ilyankou <ilyankoffslava@gmail.com>
2025-02-11 10:16:17 +0300

    реализация outbox version 1

--- payment-service/src/main/java/com/aston/payment_service/repository/OutboxRepository.java
+++ payment-service/src/main/java/com/aston/payment_service/repository/OutboxRepository.java
@@ -5,7 +5,10 @@ import com.aston.payment_service.entity.enums.OutboxStatus;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,4 +17,8 @@ public interface OutboxRepository extends JpaRepository<Outbox, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("FROM Outbox o  WHERE o.status = :status")
     List<Outbox> findWaitingOutbox(OutboxStatus status);
+
+    @Modifying
+    @Query("UPDATE Outbox o SET o.status = :value WHERE o.id IN :ids")
+    int updateStatus(@Param("value") OutboxStatus value, @Param("ids") List<Long> ids);
 }
